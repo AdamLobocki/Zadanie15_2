5986865d2fa3dc070290905f1729ed02de78a688
Adam <adamlobocki@gmail.com>
2019-06-25 15:42:31 +0200

    Zadanie zrobione naprawione

--- src/CarQueue.java
+++ src/CarQueue.java
@@ -12,14 +12,14 @@ public class CarQueue {
         Queue<Car> carList = new LinkedList<>();
         for (; number != 0; ) {
             switch (number) {
-                case 0:
-                    break;
                 case 1:
                     scanner.nextLine();
                     System.out.println("Podaj dane samochodu");
                     carList.offer(new Car(scanner.nextLine(), scanner.nextLine(), scanner.nextLine(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
+                    break;
                 case 2:
                     carList.poll();
+                    break;
             }
             System.out.println("Podaj liczbe 0, 1 lub 2");
             number = scanner.nextInt();
